cbf7db344ac766b595d714eb9dc070f4697f3bb2
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-08-01 12:06:17 +0100

    Annotation Zero

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This script contains a simple function to add two numbers.
+
+Function Documentation:
+The script includes a function `add` that takes two
+floating-point numbers and returns their sum.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two numbers and returns the result.
+
+    Parameters:
+    a (float): The first number to add.
+    b (float): The second number to add.
+
+    Returns:
+    float: The sum of the two numbers.
+
+    Example:
+    >>> add(1.2, 2.3)
+    3.5
+    """
+    return a + b
