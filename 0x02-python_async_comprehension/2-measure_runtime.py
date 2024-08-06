73a573da903f4e6f34b24baadb0537398be4cac3
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-08-06 17:38:59 +0100

    Four Coroutines

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -30,7 +30,7 @@ async def measure_runtime() -> float:
         asyncio.gather for concurrent execution of coroutines.
     """
     start_time = time.time()
-    tasks = [asyncio.create_task(async_comprehension())
+    tasks: List[asyncio.Task] = [asyncio.create_task(async_comprehension())
                                  for _ in range(4)]
     await asyncio.gather(*tasks)
     end_time = time.time()
