95c0419d4df1a7ed4edc1cc0cf0fd2ffd61e33b0
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-08-06 13:47:06 +0100

    Async Generator - Task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+This module demonstrates an asynchronous generator in Python.
+
+The shebang line above specifies the interpreter to be used for executing
+this script. It uses the env command to locate the Python 3 interpreter
+in the user's environment.
+"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    An asynchronous generator that yields random float values.
+
+    This coroutine generates 10 random float values between 0 and 10,
+    with a 1-second delay between each yield.
+
+    Yields:
+        float: A random value between 0 and 10.
+
+    Note:
+        This function uses asyncio.sleep() to simulate an asynchronous delay,
+        making it suitable for demonstrating asynchronous programming concepts.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
