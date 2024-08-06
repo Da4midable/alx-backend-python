d5c9a2810603614a560b1adad0713f2aa8030bce
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-08-06 14:42:19 +0100

    Async Generator - Task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+"""
+This module demonstrates an asynchronous list comprehension in Python.
+
+The shebang line above specifies the interpreter to be used for executing
+this script. It uses the env command to locate the Python 3 interpreter
+in the user's environment.
+"""
+
+from typing import AsyncGenerator
+import asyncio
+import random
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> AsyncGenerator[float]:
+    """
+    Collects 10 random numbers using async comprehension over async_generator.
+
+    This coroutine uses an asynchronous list comprehension to collect values
+    yielded by the async_generator coroutine.
+
+    Returns:
+        list: A list of 10 random float values between 0 and 10.
+
+    Note:
+        This function demonstrates the use of async comprehensions, which allow
+        for concise creation of collections from asynchronous generators.
+    """
+    return [result async for result in async_generator()]
